afe810228a063f91e54b613e082b29857e21dbcc
sing <si@gmail.com>
2019-12-24 20:36:25 +1100

    Section 16 end of 77

--- src/main/java/com/sing/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/sing/mobileappws/MobileAppWsApplication.java
@@ -3,11 +3,25 @@ package com.sing.mobileappws;
 import com.sing.mobileappws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+    // Required to use it as a WAR file without tomcat server and update pom xml as well
+    // <packaging>war</packaging>
+    // <dependency>
+    //			<groupId>org.springframework.boot</groupId>
+    //			<artifactId>spring-boot-starter-tomcat</artifactId>
+    //			<scope>provided</scope>
+    //	</dependency>
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MobileAppWsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MobileAppWsApplication.class, args);
